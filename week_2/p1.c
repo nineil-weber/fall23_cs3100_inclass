f3db908df28536890e6c59b6dea32d73bfcd3a64
nineil-weber <109441984+nineil-weber@users.noreply.github.com>
2023-08-31 14:08:57 -0600

    Add week2

---
+++ week_2/p1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d)[rc:%d]\n", (int) getpid(), rc);
+    } else {
+        // parent goes down this path (original process)
+        printf("hello, I am parent of %d (pid:%d)[rc: %d]\n", rc, (int) getpid(), rc);
+    }
+    return 0;
+}
